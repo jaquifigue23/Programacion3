b02f8ae96457b7a1ccb822a21659b602c1cb34fa
Oscar <ol.gt.1997@gmail.com>
2024-02-16 22:39:16 -0600

    Clase siendo creada doblemente enlazada

--- ListaDoblementeEnlazada.py
+++ ListaDoblementeEnlazada.py
@@ -1,3 +1,34 @@
-class ListaDoblementeEnlazada:
+import NodoDoblementeEnlazada
 
-    """ Cambio de codigo """
+class ListaDoblementeEnlazada:
+    def __init__(self):
+        self.cabeza = None
+    
+    def insertar_al_final(Self, nuevoNodo):
+        if (Self.cabeza == None):
+            Self.cabeza = nuevoNodo
+        else:
+            tmp = Self.cabeza
+            while(tmp == Self.cabeza or tmp.siguiente != None):
+                nuevoNodo.anterior = tmp
+                tmp.siguiente = nuevoNodo
+                tmp = tmp.siguiente                
+                
+
+    def imprimirLista(Self):
+            tmp = Self.cabeza
+            
+            while(tmp != None):
+                print(tmp.nombre + tmp.apellido + tmp.carnet)
+                tmp = tmp.siguiente
+               
+nuevaLista = ListaDoblementeEnlazada()
+
+nuevoNodo1 = NodoDoblementeEnlazada.NodoDoblementeEnlazada("oscar", "lopez", "123")
+nuevoNodo2 = NodoDoblementeEnlazada.NodoDoblementeEnlazada("jaqui", "lopez", "123")
+
+
+nuevaLista.insertar_al_final(nuevoNodo1)
+nuevaLista.insertar_al_final(nuevoNodo2)
+
+nuevaLista.imprimirLista()
---
+++ NodoDoblementeEnlazada.py
@@ -0,0 +1,8 @@
+class NodoDoblementeEnlazada:
+    def __init__(Self, nombre, apellido, carnet):
+        Self.nombre = nombre
+        Self.apellido = apellido
+        Self.carnet = carnet
+        Self.siguiente = None
+        Self.anterior = None
+
